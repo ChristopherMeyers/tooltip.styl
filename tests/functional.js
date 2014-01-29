a368763bf49958a7c574cf2fe81663db589c9fd2
Chris Meyers <chris.meyers@eqis.com>
2014-01-29 14:47:42 -0800

    adding test for other div children in tooltip
    other children that aren't the tooltip should not be styled

--- tests/functional.js
+++ tests/functional.js
@@ -1,11 +1,12 @@
 var x = require('casper').selectXPath;
 
-casper.test.begin('basic tooltip is functional', 4, function(test) {
+casper.test.begin('basic tooltip is functional', 5, function(test) {
     casper.start("tests/functional.html", function() {
         test.assertExists(x('/html/body/div'));
         test.assertSelectorHasText(x('/html/body/div'), 'Hover over me!');
-        test.assertSelectorHasText(x('/html/body/div/div'), 'Tooltip content!');
-        test.assertNotVisible(x('/html/body/div/div'));
+        test.assertSelectorHasText(x('/html/body/div/div[2]'), 'Not tooltip content!');
+        test.assertSelectorHasText(x('/html/body/div/div[1]'), 'Tooltip content!');
+        test.assertNotVisible(x('/html/body/div/div[1]'));
     }).run(function() {
         test.done();
     });
