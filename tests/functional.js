e93ea3d7a261b446b037dfc0e5276060afaa3cb9
Chris Meyers <chris.meyers@eqis.com>
2014-01-29 15:08:31 -0800

    testing visibility of the tooltip
    moving the mouse over and then away from the target

--- tests/functional.js
+++ tests/functional.js
@@ -1,11 +1,17 @@
 var x = require('casper').selectXPath;
+var mouse = require('mouse').create(casper);
 
-casper.test.begin('basic tooltip is functional', 5, function(test) {
+casper.test.begin('basic tooltip is functional', 7, function(test) {
     casper.start("tests/functional.html", function() {
         test.assertExists(x('/html/body/div'));
         test.assertSelectorHasText(x('/html/body/div'), 'Hover over me!');
         test.assertSelectorHasText(x('/html/body/div/div[2]'), 'Not tooltip content!');
         test.assertSelectorHasText(x('/html/body/div/div[1]'), 'Tooltip content!');
+        this.mouse.move(500, 500);
+        test.assertNotVisible(x('/html/body/div/div[1]'));
+        this.mouse.move(50, 10);
+        test.assertVisible(x('/html/body/div/div[1]'));
+        this.mouse.move(500, 100);
         test.assertNotVisible(x('/html/body/div/div[1]'));
     }).run(function() {
         test.done();
