78171d59693c8717c2ecb5f8186c7a28b20a4fcd
Chris Meyers <chris.meyers@eqis.com>
2014-01-29 15:09:14 -0800

    fixing order of tests to better reflect dom

--- tests/functional.js
+++ tests/functional.js
@@ -5,8 +5,8 @@ casper.test.begin('basic tooltip is functional', 7, function(test) {
     casper.start("tests/functional.html", function() {
         test.assertExists(x('/html/body/div'));
         test.assertSelectorHasText(x('/html/body/div'), 'Hover over me!');
-        test.assertSelectorHasText(x('/html/body/div/div[2]'), 'Not tooltip content!');
         test.assertSelectorHasText(x('/html/body/div/div[1]'), 'Tooltip content!');
+        test.assertSelectorHasText(x('/html/body/div/div[2]'), 'Not tooltip content!');
         this.mouse.move(500, 500);
         test.assertNotVisible(x('/html/body/div/div[1]'));
         this.mouse.move(50, 10);
