70b4466f438ec5043a93df93f16b0086b9c82a66
Chris Meyers <chris.meyers@eqis.com>
2014-01-29 15:10:41 -0800

    testing dummy non-tooltip element
    ensuring that the dummy element's visibility is not toggled on/off

--- tests/functional.js
+++ tests/functional.js
@@ -1,18 +1,24 @@
 var x = require('casper').selectXPath;
 var mouse = require('mouse').create(casper);
 
-casper.test.begin('basic tooltip is functional', 7, function(test) {
+casper.test.begin('basic tooltip is functional', 10, function(test) {
     casper.start("tests/functional.html", function() {
         test.assertExists(x('/html/body/div'));
         test.assertSelectorHasText(x('/html/body/div'), 'Hover over me!');
         test.assertSelectorHasText(x('/html/body/div/div[1]'), 'Tooltip content!');
         test.assertSelectorHasText(x('/html/body/div/div[2]'), 'Not tooltip content!');
+
         this.mouse.move(500, 500);
         test.assertNotVisible(x('/html/body/div/div[1]'));
+        test.assertVisible(x('/html/body/div/div[2]'));
+
         this.mouse.move(50, 10);
         test.assertVisible(x('/html/body/div/div[1]'));
+        test.assertVisible(x('/html/body/div/div[2]'));
+
         this.mouse.move(500, 100);
         test.assertNotVisible(x('/html/body/div/div[1]'));
+        test.assertVisible(x('/html/body/div/div[2]'));
     }).run(function() {
         test.done();
     });
