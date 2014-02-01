0fd2de6a35140f19742457d4bf6ac8a2cb8bc69a
Chris Meyers <chris.meyers@eqis.com>
2014-01-31 16:27:43 -0800

    assertion messages
    assertions are explicit about intent

--- tests/functional.js
+++ tests/functional.js
@@ -3,22 +3,22 @@ var mouse = require('mouse').create(casper);
 
 casper.test.begin('basic tooltip is functional', 10, function(test) {
     casper.start("tests/functional.html", function() {
-        test.assertExists(x('/html/body/div'));
-        test.assertSelectorHasText(x('/html/body/div'), 'Hover over me!');
-        test.assertSelectorHasText(x('/html/body/div/div[1]'), 'Tooltip content!');
-        test.assertSelectorHasText(x('/html/body/div/div[2]'), 'Not tooltip content!');
+        test.assertExists(x('/html/body/div'), "tooltip target exists");
+        test.assertSelectorHasText(x('/html/body/div'), 'Hover over me!', "valid tooltip target content");
+        test.assertSelectorHasText(x('/html/body/div/div[1]'), 'Tooltip content!', "tooltip content is correct");
+        test.assertSelectorHasText(x('/html/body/div/div[2]'), 'Not tooltip content!', "unmarked div exists");
 
         this.mouse.move(500, 500);
-        test.assertNotVisible(x('/html/body/div/div[1]'));
-        test.assertVisible(x('/html/body/div/div[2]'));
+        test.assertNotVisible(x('/html/body/div/div[1]'), "tooltip not shown initially");
+        test.assertVisible(x('/html/body/div/div[2]'), "unmarked div unaffected");
 
         this.mouse.move(50, 10);
-        test.assertVisible(x('/html/body/div/div[1]'));
-        test.assertVisible(x('/html/body/div/div[2]'));
+        test.assertVisible(x('/html/body/div/div[1]'), "tooltip visible on hover");
+        test.assertVisible(x('/html/body/div/div[2]'), "unmarked div unaffected");
 
         this.mouse.move(500, 100);
-        test.assertNotVisible(x('/html/body/div/div[1]'));
-        test.assertVisible(x('/html/body/div/div[2]'));
+        test.assertNotVisible(x('/html/body/div/div[1]'), "tooltip hidden when mouse leaves");
+        test.assertVisible(x('/html/body/div/div[2]'), "unmarked div unaffected");
     }).run(function() {
         test.done();
     });
