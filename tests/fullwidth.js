905046d5ca330d7ee4e86d87d833144b565adc88
Chris Meyers <chris.meyers@eqis.com>
2014-02-06 10:53:23 -0800

    indentation fixed
    purely formatting, going from 4 space indentation to 2 space indentation

--- tests/fullwidth.js
+++ tests/fullwidth.js
@@ -1,25 +1,25 @@
 var x = require('casper').selectXPath;
 
 casper.test.begin('large full aligned tooltip is positioned and sized correctly', 2, function(test) {
-    casper.start("tests/fullwidth.html", function() {
-        var tooltipBounds = this.getElementBounds(x('/html/body/div[1]/div'));
-        var containerBounds = this.getElementBounds(x('/html/body/div[1]'));
+  casper.start("tests/fullwidth.html", function() {
+    var tooltipBounds = this.getElementBounds(x('/html/body/div[1]/div'));
+    var containerBounds = this.getElementBounds(x('/html/body/div[1]'));
 
-        test.assertEquals(tooltipBounds['left'], containerBounds['left'], "tooltip left aligned to target");
-        test.assertEquals(tooltipBounds['width'], containerBounds['width'], "tooltip full width of target");
-    }).run(function() {
-        test.done();
-    });
+    test.assertEquals(tooltipBounds['left'], containerBounds['left'], "tooltip left aligned to target");
+    test.assertEquals(tooltipBounds['width'], containerBounds['width'], "tooltip full width of target");
+  }).run(function() {
+    test.done();
+  });
 });
 
 casper.test.begin('small full aligned tooltip is positioned and sized correctly', 2, function(test) {
-    casper.start("tests/fullwidth.html", function() {
-        var tooltipBounds = this.getElementBounds(x('/html/body/div[2]/div'));
-        var containerBounds = this.getElementBounds(x('/html/body/div[2]'));
+  casper.start("tests/fullwidth.html", function() {
+    var tooltipBounds = this.getElementBounds(x('/html/body/div[2]/div'));
+    var containerBounds = this.getElementBounds(x('/html/body/div[2]'));
 
-        test.assertEquals(tooltipBounds['left'], containerBounds['left'], "tooltip left aligned to target");
-        test.assert(tooltipBounds['width'] < containerBounds['width'], "tooltip width smaller than target");
-    }).run(function() {
-        test.done();
-    });
+    test.assertEquals(tooltipBounds['left'], containerBounds['left'], "tooltip left aligned to target");
+    test.assert(tooltipBounds['width'] < containerBounds['width'], "tooltip width smaller than target");
+  }).run(function() {
+    test.done();
+  });
 });
--- tests/functional.js
+++ tests/functional.js
@@ -2,39 +2,39 @@ var x = require('casper').selectXPath;
 var mouse = require('mouse').create(casper);
 
 casper.test.begin('basic tooltip is functional', 10, function(test) {
-    casper.start("tests/functional.html", function() {
-        test.assertExists(x('/html/body/div'), "tooltip target exists");
-        test.assertSelectorHasText(x('/html/body/div'), 'Hover over me!', "valid tooltip target content");
-        test.assertSelectorHasText(x('/html/body/div/div[1]'), 'Tooltip content!', "tooltip content is correct");
-        test.assertSelectorHasText(x('/html/body/div/div[2]'), 'Not tooltip content!', "unmarked div exists");
+  casper.start("tests/functional.html", function() {
+    test.assertExists(x('/html/body/div'), "tooltip target exists");
+    test.assertSelectorHasText(x('/html/body/div'), 'Hover over me!', "valid tooltip target content");
+    test.assertSelectorHasText(x('/html/body/div/div[1]'), 'Tooltip content!', "tooltip content is correct");
+    test.assertSelectorHasText(x('/html/body/div/div[2]'), 'Not tooltip content!', "unmarked div exists");
 
-        this.mouse.move(500, 500);
-        test.assertNotVisible(x('/html/body/div/div[1]'), "tooltip not shown initially");
-        test.assertVisible(x('/html/body/div/div[2]'), "unmarked div unaffected");
+    this.mouse.move(500, 500);
+    test.assertNotVisible(x('/html/body/div/div[1]'), "tooltip not shown initially");
+    test.assertVisible(x('/html/body/div/div[2]'), "unmarked div unaffected");
 
-        this.mouse.move(50, 10);
-        test.assertVisible(x('/html/body/div/div[1]'), "tooltip visible on hover");
-        test.assertVisible(x('/html/body/div/div[2]'), "unmarked div unaffected");
+    this.mouse.move(50, 10);
+    test.assertVisible(x('/html/body/div/div[1]'), "tooltip visible on hover");
+    test.assertVisible(x('/html/body/div/div[2]'), "unmarked div unaffected");
 
-        this.mouse.move(500, 100);
-        test.assertNotVisible(x('/html/body/div/div[1]'), "tooltip hidden when mouse leaves");
-        test.assertVisible(x('/html/body/div/div[2]'), "unmarked div unaffected");
-    }).run(function() {
-        test.done();
-    });
+    this.mouse.move(500, 100);
+    test.assertNotVisible(x('/html/body/div/div[1]'), "tooltip hidden when mouse leaves");
+    test.assertVisible(x('/html/body/div/div[2]'), "unmarked div unaffected");
+  }).run(function() {
+    test.done();
+  });
 });
 
 casper.test.begin('tooltip is positioned', 2, function(test) {
-    casper.start("tests/functional.html", function() {
-        var tooltipBounds = this.getElementBounds(x('/html/body/div/div[1]'));
-        var containerBounds = this.getElementBounds(x('/html/body/div'));
-        var tooltipCenter = tooltipBounds['left']+(tooltipBounds['width']/2);
-        var containerCenter = containerBounds['left']+(containerBounds['width']/2);
+  casper.start("tests/functional.html", function() {
+    var tooltipBounds = this.getElementBounds(x('/html/body/div/div[1]'));
+    var containerBounds = this.getElementBounds(x('/html/body/div'));
+    var tooltipCenter = tooltipBounds['left']+(tooltipBounds['width']/2);
+    var containerCenter = containerBounds['left']+(containerBounds['width']/2);
 
-        test.assertEquals(~~(tooltipCenter), ~~(containerCenter), "tooltip centered");
+    test.assertEquals(~~(tooltipCenter), ~~(containerCenter), "tooltip centered");
 
-        test.assertEquals(tooltipBounds['top']+tooltipBounds['height'], containerBounds['top'], "tooptip above target");
-    }).run(function() {
-        test.done();
-    });
+    test.assertEquals(tooltipBounds['top']+tooltipBounds['height'], containerBounds['top'], "tooptip above target");
+  }).run(function() {
+    test.done();
+  });
 });
